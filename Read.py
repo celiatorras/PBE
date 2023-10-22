cbceee6610f959d429d8ac2985fa9ad33aa33f36
celiatorras <145282982+celiatorras@users.noreply.github.com>
2023-10-22 18:55:29 +0200

    Update Read.py

--- Read.py
+++ Read.py
@@ -1,11 +1,21 @@
-import RPi.GPIO as GPIO
+mport RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
 
-reader = SimpleMFRC522()
+class RFIDReader:
+        def __init__(self):
+                self.reader = SimpleMFRC522()
 
-try:
-      id,text = reader.read()
-      print(id)
-      print(text)
-finally:
-      GPIO.cleanup()
+        def read_card(self):
+                try:
+                        uid = self.reader.read_uid()
+                        hex_id = format(int.from_bytes(uid, byteorder='big'),'X>
+                        return hex_id
+                finally:
+                        GPIO.cleanup()
+def main():
+        reader = RFIDReader()
+        hex_id = reader.read_card()
+        print(hex_id)
+
+if __name__=="__main__":
+        main()
